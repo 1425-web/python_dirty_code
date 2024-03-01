da6b1370a1ccd80c6fd9abbb44eabcaf64fa79c0
Amissay <GFHdragon@yandex.ru>
2024-03-01 19:43:37 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ print(f"Привет, {name}!")
 
 filename = "sample.txt"
 try:
-    with open(filename, 'r') as file:
-        File1 = FileInfo.F_Info(FileInfo.CntWords(filename),
-                                FileInfo.CntSentences(filename),
-                                FileInfo.RepeatWords(filename))
-        print(f"Количество слов в файле: {File1.words}")
-        print(f"Количество предложений в файле: {File1.sentences}")
-        print(f"Самые часто встречающиеся слова: {File1.repeatWords}")
+    open(filename, 'r')
+    File1 = FileInfo.F_Info(FileInfo.CntWords(filename),
+                            FileInfo.CntSentences(filename),
+                            FileInfo.RepeatWords(filename))
+    print(f"Количество слов в файле: {File1.words}")
+    print(f"Количество предложений в файле: {File1.sentences}")
+    print(f"Самые часто встречающиеся слова: {File1.repeatWords}")
 except FileNotFoundError:
     print(f"Файл '{filename}' не найден.")
