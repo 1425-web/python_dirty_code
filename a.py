e65456780d7c35c9b6e31c6c250a7ce53bcf3599
AAA <AAA>
2024-03-01 20:50:28 +0300

    main1

---
+++ a.py
@@ -0,0 +1,3 @@
+from main import filee
+
+a = filee('test.txt')
--- main.py
+++ main.py
@@ -13,7 +13,32 @@ try:
         word_count = {}
         for word in words:
             word_count[word] = word_count.get(word, 0) + 1
-        sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+        sorted_words = sorted(word_count.items(),
+                              key=lambda x: x[1], reverse=True)
         print(f"Самые часто встречающиеся слова: {sorted_words[:5]}")
 except FileNotFoundError:
     print(f"Файл '{filename}' не найден.")
+
+
+def get_txt(filename):
+    content = ''
+    with open(filename, 'r') as file:
+        content = file.read()
+    return content
+
+
+def split(str):
+    return str.split
+
+
+class filee:
+    content = ''
+    words = []
+
+    def __init__(self, filename):
+        with open(filename, 'r') as file:
+            self.content = file.read()
+            self.words = content.split()
+
+    def get_words():
+        return words
