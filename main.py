e8786837ee1434f23e5268dee7833510e2bc8e44
Amissay <GFHdragon@yandex.ru>
2024-03-01 23:34:32 +0300

    Added Cnt_Vowels and Cnt_Consonans functions

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ try:
     open(filename, 'r')
     File1 = FileInfo.F_Info(FileInfo.CntWords(filename),
                             FileInfo.CntSentences(filename),
-                            FileInfo.RepeatWords(filename))
+                            FileInfo.RepeatWords(filename),
+                            FileInfo.Cnt_Vowels(filename),
+                            FileInfo.Cnt_Consonants(filename))
     print(f"Количество слов в файле: {File1.words}")
     print(f"Количество предложений в файле: {File1.sentences}")
     print(f"Самые часто встречающиеся слова: {File1.repeatWords}")
+    print(f"Кол-во гласных в файле: {File1.vowels}")
+    print(f"Кол-во согласных в файле: {File1.consonants}")
 except FileNotFoundError:
     print(f"Файл '{filename}' не найден.")
