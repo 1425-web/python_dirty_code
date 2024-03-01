740da62223dcfb0efcfe2b8c060ecbc709a58228
Amissay <GFHdragon@yandex.ru>
2024-03-01 23:35:39 +0300

    Added Cnt_Vowels and Cnt_Consonans functions

--- FileInfo.py
+++ FileInfo.py
@@ -1,19 +1,25 @@
 class F_Info():
     """информация файла"""
-    def __init__(self,words,sentences,repeatWords):
+
+    def __init__(self, words, sentences, repeatWords, vowels, consonants):
         self.words = words
         self.sentences = sentences
         self.repeatWords = repeatWords
+        self.vowels = vowels
+        self.consonants = consonants
+
 
 def CntWords(filename):
     """подсчёт слов"""
     with open(filename, 'r') as file:
         return len(file.read().split())
 
+
 def CntSentences(filename):
     """подсчёт предложений"""
     with open(filename, 'r') as file:
         return len(file.read().split('.'))
+
 
 def RepeatWords(filename):
     """список часто повторяющихся слов"""
@@ -21,8 +27,28 @@ def RepeatWords(filename):
         words = file.read().split()
         word_count = {}
         for word in words:
-                word_count[word] = word_count.get(word, 0) + 1
+            word_count[word] = word_count.get(word, 0) + 1
         sorted_words = sorted(word_count.items(),
-                                key=lambda x: x[1],
-                                reverse=True)
+                              key=lambda x: x[1],
+                              reverse=True)
         return sorted_words[:5]
+
+
+def Cnt_Vowels(filename):
+    vowels = "eyuioaEYUIOA"
+    cnt = 0
+    with open(filename, 'r') as file:
+        symbs = file.read()
+        for symb in symbs:
+            if symb in vowels: cnt += 1
+    return cnt
+
+
+def Cnt_Consonants(filename):
+    consonants = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM"
+    cnt = 0
+    with open(filename, 'r') as file:
+        symbs = file.read()
+        for symb in symbs:
+            if symb in consonants: cnt += 1
+    return cnt
