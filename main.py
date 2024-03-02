44f86e962e8d62f7d7acfd17153f72ef2e1a9612
Amissay <GFHdragon@yandex.ru>
2024-03-02 10:51:53 +0300

    corrected mistake

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ File1 = FileInfo.F_Info(filename, FileInfo.cnt_words(content),
                         FileInfo.cnt_sentences(content),
                         FileInfo.repeat_words(content),
                         FileInfo.cnt_vowels(content),
-                        FileInfo.rnt_consonants(content))
+                        FileInfo.cnt_consonants(content))
 print(f"Имя файла: {File1.filename}")
 print(f"Количество слов в файле: {File1.words}")
 print(f"Количество предложений в файле: {File1.sentences}")
