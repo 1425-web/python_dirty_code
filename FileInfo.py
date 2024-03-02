cfd1693e14c8ff0e102190aeb1965155f631533b
Amissay <GFHdragon@yandex.ru>
2024-03-02 10:45:05 +0300

    Edit fileInfo.py

--- FileInfo.py
+++ FileInfo.py
@@ -1,7 +1,8 @@
 class F_Info():
     """информация файла"""
 
-    def __init__(self, filename, words, sentences, repeatWords, vowels, consonants):
+    def __init__(self, filename, words, sentences, repeatWords, vowels,
+                 consonants):
         self.filename = filename
         self.words = words
         self.sentences = sentences
@@ -10,48 +11,39 @@ class F_Info():
         self.consonants = consonants
 
 
-def CntWords(filename):
+def Cnt_words(content):
     """подсчёт слов"""
-    with open(filename, 'r') as file:
-        return len(file.read().split())
+    return len(content.split())
 
 
-def CntSentences(filename):
+def Cnt_sentences(content):
     """подсчёт предложений"""
-    with open(filename, 'r') as file:
-        return len(file.read().split('.'))
+    return len(content.split('.'))
 
 
-def RepeatWords(filename):
+def Repeat_words(content):
     """список часто повторяющихся слов"""
-    with open(filename, 'r') as file:
-        words = file.read().split()
-        word_count = {}
-        for word in words:
-            word_count[word] = word_count.get(word, 0) + 1
-        sorted_words = sorted(word_count.items(),
-                              key=lambda x: x[1],
-                              reverse=True)
-        return sorted_words[:5]
+    words = content.split()
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+    return sorted_words[:5]
 
 
-def Cnt_Vowels(filename):
+def Cnt_vowels(content):
     """подсчёт гласных"""
     vowels = "eyuioaEYUIOA"
     cnt = 0
-    with open(filename, 'r') as file:
-        symbs = file.read()
-        for symb in symbs:
-            if symb in vowels: cnt += 1
+    for symb in content:
+        if symb in vowels: cnt += 1
     return cnt
 
 
-def Cnt_Consonants(filename):
+def Cnt_consonants(content):
     """подсчёт согласных"""
     consonants = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM"
     cnt = 0
-    with open(filename, 'r') as file:
-        symbs = file.read()
-        for symb in symbs:
-            if symb in consonants: cnt += 1
+    for symb in content:
+        if symb in consonants: cnt += 1
     return cnt
--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import FileInfo
 print("Привет! Это плохо написанная программа.")
 name = input("Введите ваше имя: ")
 print(f"Привет, {name}!")
-
+content = ""
 filename = "sample.txt"
 try:
-    open(filename, 'r')
-    File1 = FileInfo.F_Info(filename, FileInfo.CntWords(filename),
-                            FileInfo.CntSentences(filename),
-                            FileInfo.RepeatWords(filename),
-                            FileInfo.Cnt_Vowels(filename),
-                            FileInfo.Cnt_Consonants(filename))
-    print(f"Имя файла: {File1.filename}")
-    print(f"Количество слов в файле: {File1.words}")
-    print(f"Количество предложений в файле: {File1.sentences}")
-    print(f"Самые часто встречающиеся слова: {File1.repeatWords}")
-    print(f"Кол-во гласных в файле: {File1.vowels}")
-    print(f"Кол-во согласных в файле: {File1.consonants}")
+    with open(filename, 'r') as file:
+        content = file.read()
+
 except FileNotFoundError:
     print(f"Файл '{filename}' не найден.")
+File1 = FileInfo.F_Info(filename, FileInfo.Cnt_words(content),
+                        FileInfo.Cnt_sentences(content),
+                        FileInfo.Repeat_words(content),
+                        FileInfo.Cnt_vowels(content),
+                        FileInfo.Cnt_vonsonants(content))
+print(f"Имя файла: {File1.filename}")
+print(f"Количество слов в файле: {File1.words}")
+print(f"Количество предложений в файле: {File1.sentences}")
+print(f"Самые часто встречающиеся слова: {File1.repeatWords}")
+print(f"Кол-во гласных в файле: {File1.vowels}")
+print(f"Кол-во согласных в файле: {File1.consonants}")
