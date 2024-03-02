50d16efcfa0740bace6d14a603a6105c108576f2
kurap1ka00 <145531632+kurap1ka00@users.noreply.github.com>
2024-03-02 09:49:58 +0300

    Create workng_w_txt.py

---
+++ workng_w_txt.py
@@ -0,0 +1,27 @@
+from txt_file_w import txt_file
+class workng_wth_text:
+
+    def count_vowels_and_consonants(self):
+        vowels = "аеёиоуыэюя"
+        consonants = "бвгджзйклмнпрстфхцчшщ"
+
+        vowel_count = sum(1 for char in self.content if char.lower() in vowels)
+        consonant_count = sum(1 for char in self.content if char.lower() in consonants)
+
+        return vowel_count, consonant_count
+
+    def __init__(self,txt_file: content.get_content()):
+        self.content = content
+        
+
+    def get_words(self):
+        return self.content.split()
+        
+    def get_sentences(self):
+        return self.content.split('.')
+        
+    def get_sort_word(self):
+        for word in self.get_words():
+            word_count[word] = word_count.get(word, 0) + 1
+        return sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+    
