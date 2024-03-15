2d1501a1a7be364a77fa51070f12b4ed530bbb55
RabBit <your_email@whatever.com>
2024-03-15 21:31:49 +0300

    Add Good

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import file_tools
+
 print("Привет! Это плохо написанная программа.")
 name = input("Введите ваше имя: ")
 print(f"Привет, {name}!")
 
 filename = "sample.txt"
-try:
-    with open(filename, 'r') as file:
-        print(f"Количество слов в файле: {file_tools.count_words(filename)}")
-        print(f"Количество предложений в файле: {file_tools.count_sentences(filename)}")
-        print(f"Самые часто встречающиеся слова: {file_tools.sort_words(filename)}")
-        print(f"Количество гласных букв: {file_tools.count_vowels(filename)}")
-        print(f"Количество согласных букв: {file_tools.count_consonants(filename)}")
-except FileNotFoundError:
-    print(f"Файл '{filename}' не найден.")
+file = file_tools.FileProcessor(filename)
+
+print(f"Количество слов в файле: {file.count_words()}")
+print(f"Количество предложений в файле: {file.count_sentences()}")
+print(f"Самые часто встречающиеся слова: {file.sort_words()}")
+print(f"Количество гласных букв: {file.count_vowels()}")
+print(f"Количество согласных букв: {file.count_consonants()}")
