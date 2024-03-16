47c9c8dfb4611f84c6579762e85eca04c4ff1c37
JunkerMeister <hero.hunter.04@mail.ru>
2024-03-16 10:35:33 +0300

    changes after comments

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-print("Привет! Это плохо написанная программа.")
-name = input("Введите ваше имя: ")
-print(f"Привет, {name}!")
+import hello
+import workWithWords
+
+hello.Hello()
 
 filename = "sample.txt"
+with open(filename, 'r') as file:
+    content = file.read()
+    words = content.split(' ')
 try:
-    with open(filename, 'r') as file:
-        content = file.read()
-        words = content.split()
-        print(f"Количество слов в файле: {len(words)}")
-        sentences = content.split('.')
-        print(f"Количество предложений в файле: {len(sentences)}")
-        word_count = {}
-        for word in words:
-            word_count[word] = word_count.get(word, 0) + 1
-        sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
-        print(f"Самые часто встречающиеся слова: {sorted_words[:5]}")
+    workWithWords.Analis_slov(words)
+    workWithWords.Analis_predloz(content)
+    workWithWords.Analis_bukv(content)
+
 except FileNotFoundError:
+
     print(f"Файл '{filename}' не найден.")
